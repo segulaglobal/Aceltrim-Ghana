efeeb6eacc515d97085768c2f4976a632bdd1c1d
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2024-07-15 08:46:04 +0000

    disables home navigation

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,47 +1,47 @@
-// core version + navigation, pagination modules:
-import Swiper from 'swiper';
-import { Navigation, Pagination, Autoplay, } from 'swiper/modules';
+// // core version + navigation, pagination modules:
+// import Swiper from 'swiper';
+// import { Navigation, Pagination, Autoplay, } from 'swiper/modules';
 
 
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay'
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/autoplay'
 
 
 
 
 // init Swiper:
-const heroSwiper = new Swiper('.hero-swiper', {
-  // configure Swiper to use modules
-  modules: [
-        Navigation, Pagination, Autoplay
-    ],
+// const heroSwiper = new Swiper('.hero-swiper', {
+//   // configure Swiper to use modules
+//   modules: [
+//         Navigation, Pagination, Autoplay
+//     ],
 
-    loop: true,
-    // effect: "fade",
+//     loop: true,
+//     // effect: "fade",
     
-    // effect: 'fade',
-    // fadeEffect: {
-    //     crossFade: true
-    // },
+//     // effect: 'fade',
+//     // fadeEffect: {
+//     //     crossFade: true
+//     // },
 
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
 
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        // dynamicBullets: true,
-    },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//         // dynamicBullets: true,
+//     },
 
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-        stopOnLastSlide: false,
-    }
+//     autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false,
+//         stopOnLastSlide: false,
+//     }
 
-});
+// });
