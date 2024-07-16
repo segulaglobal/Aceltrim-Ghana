dacb5981bd09d0b2a41fde40e44790c5668264f1
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2024-07-16 16:26:54 +0000

    animations added

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,2 +1,41 @@
 import 'animate.css';
 
+
+
+
+
+// Animate on Scroll with Intersection observers
+const fadeUps = document.querySelectorAll(".fade-up");
+const slideFromLefts = document.querySelectorAll(".slide-fleft");
+const slideFromRights = document.querySelectorAll(".slide-fright");
+const option = { 
+    threshold: 0.4
+}
+
+const scrollObserver = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+            else {
+                entry.target.classList.remove('show');
+                scrollObserver.unobserve;
+            }
+    })
+}, option
+);
+
+
+
+
+
+
+fadeUps.forEach(fadeUp => {
+    scrollObserver.observe(fadeUp);
+})
+slideFromLefts.forEach(slideFromLeft => {
+    scrollObserver.observe(slideFromLeft);
+})
+slideFromRights.forEach(slideFromRight => {
+    scrollObserver.observe(slideFromRight);
+})
