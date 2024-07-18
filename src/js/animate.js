7193199be716bfbb8f3802b4d33e03ddfdeac45d
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2024-07-18 17:18:16 +0000

    javascript updated

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,41 +1,18 @@
 import 'animate.css';
 
-
-
-
-
 // Animate on Scroll with Intersection observers
-const fadeUps = document.querySelectorAll(".fade-up");
-const slideFromLefts = document.querySelectorAll(".slide-fleft");
-const slideFromRights = document.querySelectorAll(".slide-fright");
-const option = { 
-    threshold: 0.4
-}
+const elementsToAnimate = document.querySelectorAll(".fade-up, .slide-fleft, .slide-fright");
+const options = { threshold: 0.4 };
 
-const scrollObserver = new IntersectionObserver ((entries) => {
+const scrollObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-            // scrollObserver.unobserve(entries);
+            scrollObserver.unobserve(entry.target); // Unobserve the target right after it is observed
         }
-            else {
-                entry.target.classList.remove('show');
-            }
-    })
-}, option
-);
+    });
+}, options);
 
-
-
-
-
-
-fadeUps.forEach(fadeUp => {
-    scrollObserver.observe(fadeUp);
-})
-slideFromLefts.forEach(slideFromLeft => {
-    scrollObserver.observe(slideFromLeft);
-})
-slideFromRights.forEach(slideFromRight => {
-    scrollObserver.observe(slideFromRight);
-})
+elementsToAnimate.forEach(element => {
+    scrollObserver.observe(element);
+});
